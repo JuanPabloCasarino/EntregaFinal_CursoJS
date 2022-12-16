725b727c4ca238c68dcab58b8143a115429046d7
Juan Pablo Casarino <juan21casarino@gmail.com>
2022-12-15 21:41:04 -0300

    Arreglo boton

--- js/script.js
+++ js/script.js
@@ -22,13 +22,53 @@ const consultarProductos = async () => {
 
 // Agrego los elementos al DOM
 
+const listaCamisetas = [consultarProductos()]
+const divProductos = document.getElementById("divProductos")
+
 consultarProductos().then(productos => {
     productos.forEach(producto => {
         divProductos.innerHTML +=
             '<div id="' + producto.id + '" class="card card-prod"><img height="300px" src="./assets/img/' + producto.imagen + '.png" class="card-img-top"> <div class="card-body"> <h4 class=card-title">' + producto.nombre + ' Talle: ' + producto.talle + '</h4> <p class="card-text">' + producto.precio + '</p> <button id=' + producto.id + ' class="btn btn-primary">Agregar</button> </div></div>'
     })
+
+    const botonAgregar = document.querySelectorAll(".btn-primary")
+    botonAgregar.forEach(boton => {
+        boton.onclick = () => {
+            console.log("hola")
+            const productoSeleccionado = listaCamisetas.find(prod => prod.id === parseInt(boton.id))
+    
+            const productosCarrito = {
+                ...productoSeleccionado,
+                cantidad: 1
+            }
+    
+            const indexCarrito = carrito.findIndex(prod => prod.id === productosCarrito.id)
+            if (indexCarrito === -1) {
+                carrito.push(productosCarrito)
+            } else {
+                carrito[indexCarrito].cantidad += 1
+            }
+            //Agrego carrito a storage
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+    
+            Toastify({
+                text: "El producto " + productosCarrito.nombre + " Talle: " + productosCarrito.talle + " ha sido agregado",
+                duration: 3000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+                onClick: function () {}
+            }).showToast();
+    
+        }
+    })
 })
-    const listaCamisetas = consultarProductos()
+    
     console.log(listaCamisetas)
 
 //Creo el array con los productos
@@ -63,42 +103,9 @@ consultarProductos().then(productos => {
 //Carrito
 const carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
-const botonAgregar = document.querySelectorAll(".btn-primary")
 
-botonAgregar.forEach(boton => {
-    boton.onclick = () => {
-        const productoSeleccionado = listaCamisetas.find(prod => prod.id === parseInt(boton.id))
 
-        const productosCarrito = {
-            ...productoSeleccionado,
-            cantidad: 1
-        }
 
-        const indexCarrito = carrito.findIndex(prod => prod.id === productosCarrito.id)
-        if (indexCarrito === -1) {
-            carrito.push(productosCarrito)
-        } else {
-            carrito[indexCarrito].cantidad += 1
-        }
-        //Agrego carrito a storage
-        localStorage.setItem("carrito", JSON.stringify(carrito))
-
-        Toastify({
-            text: "El producto " + productosCarrito.nombre + " Talle: " + productosCarrito.talle + " ha sido agregado",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            gravity: "top",
-            position: "right",
-            stopOnFocus: true,
-            style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function () {}
-        }).showToast();
-
-    }
-})
 
 //Boton finalizar total
 const botonFinalizar = document.querySelector("#finalizar")
